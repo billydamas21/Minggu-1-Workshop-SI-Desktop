c6c7f39ff0ffd202d474866272d3388363f4b0fc
billydamas21 <68609349+billydamas21@users.noreply.github.com>
2021-03-16 23:42:22 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,21 @@
+package person;
+
+/**
+ *
+ * @author Billy Damas_E41202029
+ */
+public class Person {
+
+    public static void main(String[] args) {
+        String fName = "Lisa";
+        String lName = "Palombo";
+        String stuStatus = "Active";
+        int stuId = 123456789;
+        
+        System.out.println("Student ID : " +stuId);
+        System.out.print("Student  Name : " +fName);
+        System.out.println(" " +lName);
+        System.out.println("Student Status : " +stuStatus);
+    }
+    
+}
